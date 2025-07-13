44a689baaf4f1954139acb2517326d70cebe8f8d
arun0701 <arun.kumar1@accolitedigital.com>
2025-07-13 20:19:37 +0530

    Final LRU cache implementation

--- DesignLRUCache/LRUCache.java
+++ DesignLRUCache/LRUCache.java
@@ -12,14 +12,17 @@ public class LRUCache{
             this.value = value;
         }
     }
-    private static int capacity;
+    private final int capacity;
     HashMap<Integer, Node> cache;
+    Node head;
+    Node tail;
     public LRUCache(int capacity){
         this.capacity = capacity;
         cache = new HashMap<>();
-        Node head = new Node(0,0);
-        Node tail = new Node(0,0);
-
+        head = new Node(0,0);
+        tail = new Node(0,0);
+        head.next = tail;
+        tail.prev = head;
     }
     public void put(int key, int value)
     {
@@ -30,12 +33,53 @@ public class LRUCache{
             node.value = value;
             remove(node);
             insertAtHead(node);
+            return;
         }
+        if(cache.size() == capacity)
+        {
+            Node lru = tail.prev;
+            remove(lru);
+            cache.remove(lru.key);
+            ;
+        }
+        node = new Node(key, value);
+        cache.put(key, node);
+        insertAtHead(node);
     }
     public void remove(Node node){
         Node before = node.prev;
         Node after = node.next;
         before.next = after;
         after.prev = before;
+    }
+    public void remove(int key)
+    {
+        Node node;
+        if(cache.containsKey(key))
+        {
+            node = cache.get(key);
+            remove(node);
+            cache.remove(key);
+        }
+    }
+    public void insertAtHead(Node node)
+    {
+        Node tmp = head.next;
+        head.next = node;
+        node.prev = head;
+        node.next = tmp;
+        tmp.prev = node;
+    }
+    public int get(int key)
+    {
+        Node node;
+        if(cache.containsKey(key))
+        {
+            node = cache.get(key);
+            remove(node);
+            insertAtHead(node);
+            return node.value;
+        }
+        return -1;
     }
 }
--- DesignLRUCache/NewJava.java
+++ DesignLRUCache/NewJava.java
@@ -5,25 +5,36 @@ import java.util.stream.Stream;
 public class NewJava {
     public static void main(String[] args)
     {
-        String s1="abc";
-        String s2="abc";
-        String s3= new String("abc");
+      //   String s1="abc";
+      //   String s2="abc";
+      //   String s3= new String("abc");
 
-      /*   System.out.println(s1 == s2);
-        System.out.println(s2==s3);
-        System.out.println(s1.equals(s3)); */
+      // /*   System.out.println(s1 == s2);
+      //   System.out.println(s2==s3);
+      //   System.out.println(s1.equals(s3)); */
 
 
       
-        ArrayList<Integer> ans = new ArrayList<>();
+      //   // ArrayList<Integer> ans = new ArrayList<>();
 
 
-        IntStream.range(1, 11).forEach(item -> {
-          //  System.out.println(item); 
-            ans.add(item);
-        });
+      //   IntStream.range(1, 11).forEach(item -> {
+      //     //  System.out.println(item); 
+      //       ans.add(item);
+      //   });
 
-        ans.stream().map(item -> item + 1).filter(item -> item %2 == 0).forEach( item -> System.out.println(item));
+      //  // ans.stream().map(item -> item + 1).filter(item -> item %2 == 0).forEach( item -> System.out.println(item));
+
+
+      LRUCache lruCache = new LRUCache(5);
+      lruCache.put(1,2);
+      lruCache.put(2,23);
+      lruCache.put(3,2);
+      lruCache.put(4,2);
+      lruCache.put(5,2);
+      lruCache.put(6,2);
+
+      System.out.println(lruCache.get(6));
 
 
     }
